d9be857880a09b4f05d0c5e7beba25eb605741df
Salman Khurshid <salmankhur73@gmail.com>
2026-08-02 20:49:40 +0530

    Add files via upload

---
+++ Assignment 1/Assignment 1.py
@@ -0,0 +1,14 @@
+# Task 1 For Pyhthon Assignment 
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if b != 0:
+    print("Division =", a / b)
+else:
+    print("Division by zero is not allowed.")
+
+print(" Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
